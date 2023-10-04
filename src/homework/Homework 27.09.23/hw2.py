acdc1d7b814cfd0abcee70647d903541f09bc9c2
Mmorgenn <marknsya951@gmail.com>
2023-10-04 21:24:39 +0300

    version 1.1

--- src/homework/Homework 27.09.23/hw2.py
+++ src/homework/Homework 27.09.23/hw2.py
@@ -1,6 +1,5 @@
-from os.path import *
-from string import *
-from csv import *
+from os.path import isfile
+from csv import DictWriter
 
 
 def start():
@@ -8,44 +7,38 @@ def start():
         file_input = str(input("Введите файл для чтения (txt): "))
         file_output = str(input("Введите файл для записи (csv): "))
         if (
-            isfile(file_output)
-            and isfile(file_input)
-            and file_input[-3:] == "txt"
-            and file_output[-3:] == "csv"
+            isfile(file_input)
+            and not isfile(file_output)
+            and file_input.endswith("txt")
+            and file_output.endswith("csv")
         ):
-            dict_words, set_words = find_dict(file_input)
-            write(file_output, dict_words, set_words)
+            words_count = find_words_count(file_input)
+            write(file_output, words_count)
+        elif isfile(file_output) and file_output.endswith("csv"):
+            print(f"Файл {file_output} уже существует!")
         else:
-            print("Файлы не были найдены! Попрбуйте еще раз")
+            print("Ошибка! Попрбуйте еще раз")
 
 
-def find_dict(file_input):
+def find_words_count(file_input):
     words_count = {}
-    words_set = set()
     with open(file_input, encoding="utf-8", mode="r") as file:
         for line in file:
-            for word in (
-                line.translate(str.maketrans("", "", punctuation)).lower().split()
-            ):
-                if word == "-":
-                    continue
-                elif word not in words_set:
-                    words_count[word] = 1
-                    words_set.add(word)
-                else:
-                    words_count[word] += 1
-    return words_count, words_set
+            line = line.rstrip().lower().split()
+            for word in line:
+                words_count[word] = words_count.get(word, 0) + 1
+    return words_count
 
 
-def write(file_output, dict_words, set_words):
+def write(file_output, words_count):
     with open(file_output, encoding="utf-8-sig", mode="w", newline="") as file:
         header = ["Word", "Count"]
         file_writer = DictWriter(file, fieldnames=header)
         file_writer.writeheader()
-        for word in set_words:
-            file_writer.writerow({"Word": word, "Count": dict_words[word]})
+        file_writer.writerows(
+            {"Word": word, "Count": words_count[word]} for word in words_count
+        )
 
 
 if __name__ == "__main__":
-    punctuation = punctuation.replace("-", "") + "\n«»…"
     start()
