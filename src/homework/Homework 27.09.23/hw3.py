f805bb0cb13876a879d9cfbb78f4df96c0910086
Mmorgenn <marknsya951@gmail.com>
2023-10-04 21:28:05 +0300

    version 1.1

--- src/homework/Homework 27.09.23/hw3.py
+++ src/homework/Homework 27.09.23/hw3.py
@@ -1,4 +1,4 @@
-from os.path import *
+from os.path import isfile
 
 
 def start():
@@ -7,56 +7,58 @@ def start():
         file_output = input("Введите файл для чтения (txt): ")
         if all(
             [
-                isfile(file_output),
                 isfile(file_input),
-                file_input[-3:] == "txt",
-                file_output[-3:] == "txt",
+                not isfile(file_output),
+                file_input.endswith("txt"),
+                file_output.endswith("txt"),
                 file_input != file_output,
             ]
         ):
-            file_scrolling(file_input, file_output)
+            dna_lines = file_scrolling(file_input)
+            file_writer(dna_lines, file_output)
+        elif isfile(file_output) and file_output.endswith("txt"):
+            print(f"Файл {file_output} уже существует!")
         else:
-            print("Файлы не были найдены! Попрбуйте еще раз")
+            print("Ошибка! Попрбуйте еще раз")
 
 
-def file_scrolling(file_name_input, file_name_output):
-    with open(file_name_input, "r") as file_input, open(
-        file_name_output, "w"
-    ) as file_output:
+def file_scrolling(file_name_input):
+    with open(file_name_input, "r") as file_input:
         m = int(file_input.readline())
         dna_line = file_input.readline()
         n = int(file_input.readline())
+        dna_lines = list()
         for i in range(n):
-            command, arg1, arg2 = "".join(
-                [i for i in str(file_input.readline()) if i not in "]\n"]
-            ).split("[")
+            command, arg1, arg2 = str(file_input.readline()).rstrip().split()
             if command == "DELETE":
-                dna_line = delete_fragment(dna_line, arg1, arg2, file_output)
+                dna_line = delete_fragment(dna_line, arg1, arg2)
             elif command == "INSERT":
-                dna_line = insert_fragment(dna_line, arg1, arg2, file_output)
+                dna_line = insert_fragment(dna_line, arg1, arg2)
             elif command == "REPLACE":
-                dna_line = replace_fragment(dna_line, arg1, arg2, file_output)
-            else:
-                file_output.write(dna_line)
+                dna_line = replace_fragment(dna_line, arg1, arg2)
+            dna_lines.append(dna_line)
+        return dna_lines
 
 
-def delete_fragment(line, start, end, file):
-    line = line.replace(start, " ", 1).split()
-    line[1] = line[1].replace(end, " ", 1).split()[1]
-    new_line = line[0] + line[1]
-    file.write(new_line + "\n")
-    return new_line
+def file_writer(dna_lines, file_name_output):
+    with open(file_name_output, "w") as file_output:
+        file_output.writelines(line for line in dna_lines)
 
 
-def insert_fragment(line, start, fragment, file):
+def delete_fragment(line, start, end):
+    start_id = line.find(start)
+    end_line = line[start_id:]
+    end_id = end_line.find(end) + len(end)
+    return line[:start_id] + end_line[end_id:]
+
+
+def insert_fragment(line, start, fragment):
     new_line = line.replace(start, start + fragment, 1)
-    file.write(new_line + "\n")
     return new_line
 
 
-def replace_fragment(line, template, fragment, file):
+def replace_fragment(line, template, fragment):
     new_line = line.replace(template, fragment, 1)
-    file.write(new_line + "\n")
     return new_line
 
 
