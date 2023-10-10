b5cd975ce62cf15c1dff24b98ac481c0386ebd6c
Mmorgenn <marknsya951@gmail.com>
2023-10-10 22:05:58 +0300

    fix random_number, lists

--- src/practice/Pr 22.09.23/pr1.py
+++ src/practice/Pr 22.09.23/pr1.py
@@ -5,9 +5,9 @@ def start():
     while True:
         n = str(input("Введи натуральное число больше 1: "))
         if n.isdigit() and n not in ["0", "1"]:
-            print(", ".join([i[1] for i in find_fractions(int(n))]))
+            print(", ".join([f"{i[0]}/{i[1]}" for i in find_fractions(int(n))]))
         else:
-            print("Ошибка! Попробуй еще раз")
+            print("Ошибка! Вы не ввели натуральное число больше 1!\nПопробуйте еще раз")
 
 
 def find_fractions(n):
@@ -15,8 +15,8 @@ def find_fractions(n):
     for i in range(1, n):
         for j in range(i + 1, n + 1):
             if gcd(i, j) == 1:
-                list_fractions.append((i / j, f"{i}/{j}"))
-    list_fractions.sort(key=lambda x: x[0])
+                list_fractions.append((i, j))
+    list_fractions.sort(key=lambda x: x[0] / x[1])
     return list_fractions
 
 
--- src/practice/Pr 22.09.23/pr2.py
+++ src/practice/Pr 22.09.23/pr2.py
@@ -1,8 +1,14 @@
-import random
+from random import shuffle
 
 
 def random_number():
-    return "".join(str(i) for i in random.sample(range(0, 10), 4))
+    random_f1 = [str(i) for i in range(1, 10)]
+    random_f0 = ["0", *random_f1]
+    shuffle(random_f1)
+    shuffle(random_f0)
+    first_num = random_f1[0]
+    random_f0.remove(first_num)
+    return [first_num, *random_f0[:3]]
 
 
 def check_num(num_orig, num_input):
@@ -10,7 +16,7 @@ def check_num(num_orig, num_input):
     for i in range(4):
         if num_orig[i] == num_input[i]:
             b_count += 1
-        elif num_input[i] in num_orig and num_input[i]:
+        elif num_input[i] in num_orig:
             c_count += 1
     return b_count, c_count
 
@@ -18,13 +24,16 @@ def check_num(num_orig, num_input):
 def game(num_orig):
     while True:
         num_input = str(input("Введите четырех значное число: "))
-        if len(num_input) == 4 and all(
-            [(num_input[i] != num_input[i + 1]) for i in range(3)]
+        if (
+            len(num_input) == 4
+            and num_input.isdigit()
+            and all([num_input.count(num_input[i]) == 1 for i in range(4)])
         ):
             b_count, c_count = check_num(num_orig, num_input)
             print(f"Быков: {b_count}, Коров: {c_count}")
             if b_count == 4:
-                print(f"Абсолютно верно! Это число - {num_orig}")
+                final_num = "".join(num_orig)
+                print(f"Абсолютно верно! Это число - {final_num}")
                 break
         else:
             print("Ошибка! Введите четырехзначное число без повторяющихся цифр")
@@ -33,7 +42,6 @@ def game(num_orig):
 def start():
     while True:
         num_orig = random_number()
-        print(num_orig)
         game(num_orig)
 
 
