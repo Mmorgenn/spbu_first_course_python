c1f1508f707850352a32a6b9131c31b561fb7ecd
Mmorgenn <marknsya951@gmail.com>
2023-10-17 00:11:53 +0300

    fix: Naming. Add checking arguments

--- src/homework/Homework 11.10.23/hw1.py
+++ src/homework/Homework 11.10.23/hw1.py
@@ -1,13 +1,16 @@
-def curry_explicit(function_name, arity: int):
-    if arity < 0:
-        print(f"Arity cannot be {arity}! Returned None")
+def curry_explicit(function, arity):
+    if not callable(function):
+        print(f"{function} is not function! Returned None")
         return None
-    if arity == 0:
-        return lambda: function_name()
+    elif not type(arity) == int or arity < 0:
+        print("Arity should be int > 0! Returned None")
+        return None
+    elif arity == 0:
+        return function()
 
     def function_on(arguments):
         if arity == len(arguments):
-            return function_name(*arguments)
+            return function(*arguments)
 
         def curry(argument):
             return function_on([*arguments, argument])
@@ -17,9 +20,12 @@ def curry_explicit(function_name, arity: int):
     return function_on([])
 
 
-def uncurry_explicit(function_name, arity: int):
-    if arity < 0:
-        print(f"Arity cannot be {arity}! Returned None")
+def uncurry_explicit(function, arity):
+    if not callable(function):
+        print(f"{function} is not function! Returned None")
+        return None
+    if not type(arity) == int or arity < 0:
+        print("Arity should be int > 0! Returned None")
         return None
 
     def uncurry(*arguments):
@@ -27,22 +33,10 @@ def uncurry_explicit(function_name, arity: int):
             print("Arity != the number of arguments! Returned None")
             return None
         elif arity == 0:
-            return function_name()
-        function_result = function_name(arguments[0])
+            return function()
+        function_result = function(arguments[0])
         for i in range(1, arity):
             function_result = function_result(arguments[i])
         return function_result
 
     return uncurry
-
-
-if __name__ == "__main__":
-    f2 = curry_explicit((lambda x, y, z: f"<{x},{y},{z}>"), 3)
-    f3 = curry_explicit((lambda x: f"<{x}>"), 1)
-    h3 = curry_explicit(print, 0)
-    g2 = uncurry_explicit(f2, 3)
-    h3()
-    print(uncurry_explicit(f3, -10))
-    print(uncurry_explicit(f3, 2)(123))
-    print(f2(123)(456)(562))
-    print(g2(123, 456, 562))
