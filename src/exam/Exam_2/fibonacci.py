d73903ad67ecc6a6d76aafdbb0af0ae279cff6bc
Mmorgenn <marknsya951@gmail.com>
2023-11-24 16:36:28 +0300

    fix: black

--- src/exam/Exam_2/fibonacci.py
+++ src/exam/Exam_2/fibonacci.py
@@ -15,9 +15,13 @@ def get_fibonacci_num(num: int) -> int:
 def main():
     user_input = input("Введите цеолое положительное число меньшее 91: ")
     if is_correct_input(user_input):
-        print(f"Число Фибоначчи под номером {user_input}: {get_fibonacci_num(int(user_input))}")
+        print(
+            f"Число Фибоначчи под номером {user_input}: {get_fibonacci_num(int(user_input))}"
+        )
     else:
-        print("Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91")
+        print(
+            "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91"
+        )
 
 
 if __name__ == "__main__":
--- tests/exam/Exam_2/test_fibonacci.py
+++ tests/exam/Exam_2/test_fibonacci.py
@@ -4,8 +4,7 @@ from src.exam.Exam_2.fibonacci import get_fibonacci_num, is_correct_input, main
 
 
 @pytest.mark.parametrize(
-    "number,expected",
-    ((12, 144), (0, 0), (1, 1), (2, 1), (75, 2111485077978050))
+    "number,expected", ((12, 144), (0, 0), (1, 1), (2, 1), (75, 2111485077978050))
 )
 def test_fibonacci(number, expected):
     function = get_fibonacci_num(number)
@@ -14,7 +13,7 @@ def test_fibonacci(number, expected):
 
 @pytest.mark.parametrize(
     "number,expected",
-    (("12", True), ("gh", False), ("1.0", False), ("-12", False), ("912", False))
+    (("12", True), ("gh", False), ("1.0", False), ("-12", False), ("912", False)),
 )
 def test_fibonacci(number, expected):
     function = is_correct_input(number)
@@ -27,9 +26,18 @@ def test_fibonacci(number, expected):
         ("12", "Число Фибоначчи под номером 12: 144\n"),
         ("0", "Число Фибоначчи под номером 0: 0\n"),
         ("90", "Число Фибоначчи под номером 90: 2880067194370816120\n"),
-        ("test", "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n"),
-        ("1.2", "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n"),
-        ("-77", "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n"),
+        (
+            "test",
+            "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n",
+        ),
+        (
+            "1.2",
+            "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n",
+        ),
+        (
+            "-77",
+            "Вы не корректно ввели число! Ваше число должно быть целым, положительным, меньше 91\n",
+        ),
     ),
 )
 def test_main(monkeypatch, user_input, expected):
@@ -38,5 +46,3 @@ def test_main(monkeypatch, user_input, expected):
     monkeypatch.setattr("sys.stdout", fake_output)
     main()
     assert fake_output.getvalue() == expected
-
-
