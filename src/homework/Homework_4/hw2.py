74dab2d296541d0dc2ab167d30c85d549f238403
Mmorgenn <marknsya951@gmail.com>
2023-11-28 01:57:43 +0300

    fix: normalize function, naming

--- src/homework/Homework_4/hw2.py
+++ src/homework/Homework_4/hw2.py
@@ -1,10 +1,13 @@
+from math import isinf, isnan
+
+
 def num_check(num):
     try:
         num_float = float(num)
     except ValueError:
         raise ValueError("It must be float number")
-    if "inf" in num:
-        raise ValueError("It should not be inf")
+    if isinf(num_float) or isnan(num_float):
+        raise ValueError("It should not be Inf or Nan")
     return num_float
 
 
@@ -18,12 +21,12 @@ def get_binary_integer(num):
     return bin_num
 
 
-def get_binary_fraction(num, precision):
-    num = num / 10 ** len(str(num))
+def get_binary_fraction(num, mantissa_bits):
+    num = int(num) / 10 ** len(num)
     bin_num = []
     if num == 0:
         return ["0"]
-    for i in range(precision):
+    for i in range(mantissa_bits):
         num *= 2
         bin_num.append(str(num // 1)[0])
         if num % 1 == 0:
@@ -32,39 +35,49 @@ def get_binary_fraction(num, precision):
     return bin_num
 
 
-def normalization(int_part, float_part):
-    p = 0
-    while len(int_part) != 0:
-        element = int_part.pop(-1)
-        float_part = [element] + float_part
-        p += 1
-    result = "0." + ("").join(float_part)
-    return [result, p]
+def get_normalised_form(int_part, fraction_part):
+    bin_number = ("").join(int_part) + "." + ("").join(fraction_part)
+    mantissa = (
+        (bin_number.replace(".", "")).lstrip("0") if float(bin_number) != 0 else "0"
+    )
+    order = (
+        bin_number.find(".")
+        if bin_number[0] != "0"
+        else -len(bin_number[2 : bin_number.find(mantissa[0])])
+    )
+    mantissa = mantissa.rstrip("0")
+    return ["0." + mantissa, order]
 
 
-def fp_conversation(num_list, range, precision):
-    shift = ("").join(get_binary_integer(int(num_list[2] - 1) + 2 ** (range - 1) - 1))
-    if len(shift) > range:
-        raise OverflowError("The number i too large for this format")
-    mantissa = num_list[1][3:] if len(num_list[1]) > 3 else num_list[1][2]
-    if len(mantissa) > precision:
-        mantissa = mantissa[:precision]
+def convert_to_fp(normalized_num, order_bits, mantissa_bits):
+    sign, mantissa, order = normalized_num
+    shifted_order = ("").join(
+        get_binary_integer((order - 1) + 2 ** (order_bits - 1) - 1)
+    )
+    if len(shifted_order) > order_bits:
+        raise OverflowError("This number too large for this format")
+
+    mantissa = mantissa[3:] if len(mantissa) > 3 else mantissa[2]
+    if len(mantissa) > mantissa_bits:
+        mantissa = mantissa[:mantissa_bits]
     else:
-        mantissa += "0" * (precision - len(mantissa))
-    sign = int(num_list[0] == "-")
-    return [sign, shift, mantissa]
+        mantissa = mantissa.ljust(mantissa_bits, "0")
+
+    sign = "0"
+    if normalized_num[0] == "-":
+        sign = "1"
+    return [sign, shifted_order, mantissa]
 
 
-def main(num, range, precision):
+def main(num, order_bits, mantissa_bits):
     sign = "-" if str(num)[0] == "-" else "+"
     int_part, float_part = (str(num)).removeprefix("-").split(".")
-    int_bin, float_bin = get_binary_integer(int(int_part)), get_binary_fraction(
-        int(float_part), precision
-    )
-    bin_num_normalization = [sign] + normalization(int_bin, float_bin)
+    int_bin = get_binary_integer(int(int_part))
+    float_bin = get_binary_fraction(float_part, mantissa_bits)
+    bin_num_normalization = [sign] + get_normalised_form(int_bin, float_bin)
     print("{}{}*2^{}".format(*bin_num_normalization))
     try:
-        fp_converted = fp_conversation(bin_num_normalization, range, precision)
+        fp_converted = convert_to_fp(bin_num_normalization, order_bits, mantissa_bits)
     except OverflowError:
         print("Это число слишком большое/маленькое для этого формата хранения числа")
         return
@@ -72,7 +85,7 @@ def main(num, range, precision):
 
 
 def start():
-    num_input = input("Ввещите вещественное число: ")
+    num_input = input("Введите вещественное число: ")
     try:
         num = num_check(num_input)
     except ValueError as e:
@@ -90,5 +103,4 @@ def start():
 
 
 if __name__ == "__main__":
-    while True:
-        start()
+    start()
