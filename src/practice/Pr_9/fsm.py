0b88c7ec40e2392092869a4345f858eda1a11ab5
Mmorgenn <marknsya951@gmail.com>
2023-12-21 22:33:46 +0300

    fix: changed the way of changing state

--- src/practice/Pr_9/fsm.py
+++ src/practice/Pr_9/fsm.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 class FSMachine:
     name: str
     states: dict[int, dict[str, int]]
-    current_state: int
+    zero_state: int
     end_states: list[int]
 
 
@@ -18,22 +18,18 @@ def create_fs_machine(
     return FSMachine(name, states, zero_state, end_state)
 
 
-def move_state(fsm: FSMachine, symbol: str) -> bool:
-    current_state = fsm.current_state
+def move_state(fsm: FSMachine, symbol: str, current_state: int) -> int | None:
     current_transfers = fsm.states[current_state]
-    for key, value in current_transfers.items():
-        if symbol in key:
-            fsm.current_state = value
-            return True
-    return False
+    for symbols, state in current_transfers.items():
+        if symbol in symbols:
+            return state
 
 
 def validate_string(fsm: FSMachine, word: str) -> bool:
-    zero_state = fsm.current_state
+    state = fsm.zero_state
     for symbol in word:
-        if not move_state(fsm, symbol):
-            fsm.current_state = zero_state
+        state = move_state(fsm, symbol, state)
+        if state is None:
             return False
-    validity = fsm.current_state in fsm.end_states
-    fsm.current_state = zero_state
+    validity = state in fsm.end_states
     return validity
